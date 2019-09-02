bb11389c53a6d4697398221a0604120e5404e09d
Albert <wuyichang1990@gmail.com>
2019-09-02 17:15:59 +0100

    pwa

---
+++ js/sw.js
@@ -0,0 +1,44 @@
+var cacheStorageKey = 'coolpanda-pwa-1'
+
+var cacheList = [
+    '/',
+    "index.html",
+    "css/ain.css",
+    "assets/images/background-cover.jpg",
+    "assets/images/avatar.jpg"
+]
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheStorageKey)
+            .then(cache => cache.addAll(cacheList))
+            .then(() => self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            if (response != null) {
+                return response
+            }
+            return fetch(e.request.url)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        Promise.all(
+            caches.keys().then(cacheNames => {
+                return cacheNames.map(name => {
+                    if (name !== cacheStorageKey) {
+                        return caches.delete(name)
+                    }
+                })
+            })
+        ).then(() => {
+            return self.clients.claim()
+        })
+    )
+})
